0ee9a835e20ffcd61250438359bbe79a95eaae10
Ross <7848161+PwnDexter@users.noreply.github.com>
2020-04-23 19:25:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-pip uninstall -y dnspython
-pip install -r requirements.txt
+pip3 uninstall -y dnspython
+pip3 install -r requirements.txt
 git clone https://github.com/aboul3la/Sublist3r.git
-touch Sublist3r/__init__.py
+cd Sublist3r
+python3 setup.py install
+pip3 install -r requirements.txt
+touch __init__.py
 exit 1
